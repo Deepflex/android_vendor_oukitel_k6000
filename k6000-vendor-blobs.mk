7f976d4a9deb2812daececcf276edb1be4502212
Maxim <hqdevnews@gmail.com>
2016-05-21 23:48:10 +0700

    Stable #1
    Stable #1

---
+++ k6000-vendor-blobs.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000/vendor,system/vendor)
